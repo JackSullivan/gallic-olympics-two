4c02c3cbb401a184a02464bebc457c27832c8fd5
John Sullivan <john.t.sullivan@gmail.com>
2014-04-04 16:52:38 -0400

    fixes

--- bin/rand-tablet.sh
+++ bin/rand-tablet.sh
@@ -4,6 +4,6 @@
 for i in `seq $1`
 do
 
-java -cp ../target/gaulish_olympics_two-1.0-SNAPSHOT-jar-with-dependencies.jar so.modernized.dos.SpawnRandomTablet 127.0.0.1 2552 $2 $3 "Rome|Gaul|Carthage" "Curling|Piathlon|Bayesball" > tablet-$i-of-$1-at-$2-asks-$3-total.log &
+java -cp ../target/gaulish_olympics_two-1.0-SNAPSHOT-jar-with-dependencies.jar so.modernized.dos.SpawnRandomTablet 127.0.0.1 5000 $2 $3 "Rome|Gaul|Carthage" "Curling|Piathlon|Bayesball" > ../results/tablet-$i-of-$1-at-$2-asks-$3-total.log &
 
 done
